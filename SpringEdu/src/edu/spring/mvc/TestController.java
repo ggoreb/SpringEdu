abb8df5ca862e2c69c6f182a619c25aa1c6b6874
ggoreb <seorab@naver.com>
2019-07-11 16:36:20 +0900

    222

--- SpringEdu/src/edu/spring/mvc/TestController.java
+++ SpringEdu/src/edu/spring/mvc/TestController.java
@@ -5,12 +5,14 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class TestController {
-	@RequestMapping("third")
+	@RequestMapping(value="third", method=RequestMethod.GET)
 	@ResponseBody
 	public String third() {
 		return "<h1>Hello</h1>";
@@ -26,12 +28,16 @@ public class TestController {
 	}
 	
 	@RequestMapping("first") //<- 접속할 주소(URL)
-	public String first(HttpServletRequest req) { // 메소드명 자유, 중복되지 않게!
+	public ModelAndView first(
+			HttpServletRequest req) { // 메소드명 자유, 중복되지 않게!
 		String key = req.getParameter("key");
 		System.out.println(key);
 		
-		return "first"; // <- View로 사용될 jsp 파일명
-		// /WEB-INF/views/first.jsp
+		// jsp view 이름과 데이터 정보를 담음
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("first");
+		mav.addObject("data", "Hello");
+		return mav;
 	}
 }
 
